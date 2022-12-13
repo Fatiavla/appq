9f2ffe308deae06e56a88fa224e933f5afb925da
VLadislav F <bioafc@mail.ru>
2022-12-13 10:23:39 +0300

    dd

--- pyt.py
+++ pyt.py
@@ -1,2 +1,17 @@
-a = int(input('Write msg'))
-print(a)
+# a = int(input('Write msg'))
+# list = [1, 3, 4, 5, 6, 7, 8]
+# list.reverse()
+# print (list)
+
+# list2 = [1, 4, 5, 6, 7, 7, 4, 3]
+# list_new = list2[:: -1]
+# print(list2)
+# print(list_new)
+
+# lisst = [1, 329, 34, 55, 26, 17, 8, 2, 5]
+# new_try = list(sorted(lisst))
+# print(lisst)
+# print(new_try)
+
+list = [1, 3, 4, 5, 6, 7, 8]
+print(list[:-1])
