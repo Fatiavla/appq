793981a7000500a938cb245b3773b6809118586f
VLadislav F <bioafc@mail.ru>
2022-12-15 13:17:35 +0300

    Comments in shpargalka

--- list from for.py
+++ list from for.py
@@ -5,12 +5,12 @@
 #     print(el)
 
 
-n = int(input('Enter length: '))
-user_list = []
+n = int(input('Enter length: ')) #просим пользователя ввести длину списка
+user_list = [] # это то, куда будем поещать список
 
 i = 0
-while i < n:
-    string = 'Enter element #' + str(i + 1) + ": "
+while i < n: #Через цикл заполняем список
+    string = 'Enter element #' + str(i + 1) + ": " # ввод списка
     user_list.append(input(string))
     i+=1
 
